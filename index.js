a1422b931ccfb10feb40ecb9dbfedfca2be905ff
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-04-15 15:14:17 -0400

    adding enpoints and documentation

--- index.js
+++ index.js
@@ -16,58 +16,67 @@ const topMovies = [
     title: 'The Shawshank Redemption',
     director: 'Frank Darabont',
     stars: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    genre: 'Drama',
   },
 
   {
     title: 'The Godfather',
     director: 'Frances Ford Coppola',
     stars: ['Marlon Brando', 'Al Pacino', 'James Caan'],
+    genre: 'Crime',
   },
   {
     title: 'The Dark Knight',
     director: 'Christopher Nolan',
     stars: ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart'],
+    genre: 'Action',
   },
   {
     title: 'The Godfather: Part II',
     director: 'Francis Ford Coppola',
     stars: ['Al Pacino', 'Robert De Niro', 'Robert Duvall'],
+    genre: 'Crime',
   },
   {
     title: '12 Angry Men',
     director: 'Sidney Lumet',
     stars: ['Henry Fonda', 'Lee J. Cobb', 'Martin Balsam'],
+    genre: 'Drama',
   },
   {
     title: "Schindler's List",
     director: 'Steven Spielberg',
     stars: ['Liam Neeson', 'Ralph Fiennes', 'Ben Kingsley'],
+    genre: 'History',
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
     director: 'Peter Jackson',
     stars: ['Elijah Wood', 'Viggo Mortensen', 'Ian McKellen'],
+    genre: 'Fantasy',
   },
   {
     title: 'Pulp Fiction',
     director: 'Quentin Tarantino',
     stars: ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson'],
+    genre: 'Crime',
   },
   {
     title: 'The Lord of the Rings: The Fellowship of the Ring',
     director: 'Peter Jackson',
     stars: ['Elijah Wood', 'Orlando Bloom', 'Ian McKellen'],
+    genre: 'Fantasy',
   },
   {
     title: 'The Good, the Bad and the Ugly',
     director: 'Sergio Leone',
     stars: ['Clint Eastwood', 'Eli Wallach', 'Lee Van Cleef'],
+    genre: 'Western',
   },
 ];
 
 //middleware - logging, static public folder, error logging
 app.use(morgan('combined', { stream: accessLogStream }));
-
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
@@ -81,6 +90,47 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+app.get('movies/:title', (req, res) => {
+  res.json(
+    topMovies.find((movie) => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+
+app.get('movies/genre/:title', (req, res) => {
+  let movie = topMovies.find((movie) => {
+    return movie.title === req.params.title;
+  });
+  if (movie) {
+    res.status(200).send(`${req.params.title} is a ${movie.genre}`);
+  } else {
+    res.status(400).send('Movie not Found');
+  }
+});
+
+app.get('/directors/:name', (req, res) => {
+  res.status(200).send(`Request recived for ${req.params.name}`);
+});
+
+app.post('/user', (req, res) => {
+  res.status(200).send(`Request recived for new user`);
+});
+
+app.put('/users/:name', (req, res) => {
+  res.status(200).send(`Request recived to update name for ${req.params.name}`);
+});
+
+app.put('/favorites/:id/:title', (req, res) => {
+  res
+    .status(200)
+    .send(`Adding ${req.params.title} to favorites for ${req.params.name}`);
+});
+
+app.delete('/users/:name', (req, res) => {
+  res.status(200).send(`Deleting user ${req.params.name}`);
 });
 
 app.get('/documentation', (req, res) => {
