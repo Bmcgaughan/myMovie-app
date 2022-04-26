2e84b7593696540ddd58536a23e1edd58d428045
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-04-26 15:34:24 -0400

    2.10 to submit

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ const app = express();
 const cors = require('cors');
 const allowedOrigins = ['http://localhost:8080'];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message = `No access from this origin ${origin}`;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+//commented out to allow all requests
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message = `No access from this origin ${origin}`;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require('./auth')(app);
 const passport = require('passport');
