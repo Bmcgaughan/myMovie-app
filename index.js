253f75078f0b4bca0c0b872f16d86a1ce4873a0b
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-05-04 07:08:41 -0400

    require token on movies

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get('/', (req, res) => {
 //return json of all movies
 app.get(
   '/movies',
-  // passport.authenticate('jwt', { session: false }),
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
